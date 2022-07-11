417e011e7ba11b9da6001822d4d2d32d95a70347
Maria Bohorquez <maria.bohorquez@ceiba.com.co>
2022-07-11 18:07:38 -0500

    add redux and create store with pokemons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import { useEffect, useState } from 'react';
-import { Col } from 'antd';
-import Searcher from './components/Searcher';
-import PokemonList from './components/PokemonList';
-import { getPokemon } from './api';
-import logo from './statics/logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { connect } from "react-redux";
+import { setPokemons as setPokemonsAction } from "./actions";
+import { Col } from "antd";
+import Searcher from "./components/Searcher";
+import PokemonList from "./components/PokemonList";
+import { getPokemon } from "./api";
+import logo from "./statics/logo.svg";
+import "./App.css";
 
-function App() {
-  const [pokemons, setPokemons] = useState([]);
+function App({ pokemons, setPokemons }) {
 
   useEffect(() => {
     const fetchPokemons = async () => {
@@ -19,9 +20,9 @@ function App() {
   }, []);
 
   return (
-    <div className='App'>
+    <div className="App">
       <Col span={4} offset={10}>
-        <img src={logo} alt='Pokedux' />
+        <img src={logo} alt="Pokedux" />
       </Col>
       <Col span={8} offset={8}>
         <Searcher />
@@ -31,4 +32,12 @@ function App() {
   );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  pokemons: state.pokemons,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setPokemons: (payload) => dispatch(setPokemonsAction(payload)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { Provider } from 'react-redux';
+import { applyMiddleware, compose, legacy_createStore as createStore } from 'redux';
+import { pokemonsReducer } from './reducers/pokemons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(pokemonsReducer, composeEnhancers(applyMiddleware()));
+
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
