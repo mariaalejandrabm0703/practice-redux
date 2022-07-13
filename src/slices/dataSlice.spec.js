6670beb2296f7b1de14b22b276d96b68ca86819c
Maria Bohorquez <maria.bohorquez@ceiba.com.co>
2022-07-12 20:32:10 -0500

    add unit test dataSlice

--- src/slices/dataSlice.spec.js
+++ src/slices/dataSlice.spec.js
@@ -1,12 +1,58 @@
-import dataSlice from "./dataSlice";
+import dataSlice, { setFavorite, setPokemons, setPokemonsFilter } from "./dataSlice";
+import {
+  dataStateWithPokemons,
+  dataStateWithPokemonsFavorite,
+  dataStateWithPokemonsFilter,
+  initialStateFixture,
+} from "./fixtures/dataFixtures";
 
 describe("dataSlice", () => {
   it("should create the correct actions", () => {
     const actions = dataSlice.actions;
 
     expect(dataSlice.name).toBe("data");
-    // expect(actions.setPokemons).toBeDefined();
-    // expect(actions.setPokemonsFilter).toBeDefined();
-    // expect(actions.setFavorite).toBeDefined();
+    expect(actions.setPokemons).toBeDefined();
+    expect(actions.setPokemonsFilter).toBeDefined();
+    expect(actions.setFavorite).toBeDefined();
   });
+
+  it("should return the correct initialState", () => {
+    const state = dataSlice.reducer(initialStateFixture, {});
+    expect(state).toEqual(initialStateFixture);
+  });
+
+  it("should return the correct pokemons", () => {
+    const state = dataSlice.reducer(dataStateWithPokemons, {});
+    expect(state).toEqual(dataStateWithPokemons);
+  });
+
+  it("should return the correct pokemonsFilter", () => {
+    const state = dataSlice.reducer(dataStateWithPokemonsFilter, {});
+    expect(state).toEqual(dataStateWithPokemonsFilter);
+  });
+
+  it("should do setPokemons correctly", () => {
+    const state = dataSlice.reducer(
+      initialStateFixture,
+      setPokemons([dataStateWithPokemons.pokemons[0]])
+    );
+    expect(state).toEqual(dataStateWithPokemons);
+  });
+
+  it("should do setPokemonsFilter correctly", () => {
+    const state = dataSlice.reducer(
+      initialStateFixture,
+      setPokemonsFilter([dataStateWithPokemonsFilter.pokemonsFilter[0]])
+    );
+    expect(state).toEqual(dataStateWithPokemonsFilter);
+  });
+
+  it("should do setFavorite correctly", () => {
+    const state = dataSlice.reducer(
+      dataStateWithPokemons,
+      setFavorite([dataStateWithPokemons.pokemons[0]])
+    );
+    expect(state).toEqual(dataStateWithPokemonsFavorite);
+  });
+
 });
--- src/slices/fixtures/dataFixtures.js
+++ src/slices/fixtures/dataFixtures.js
@@ -1,10 +1,9 @@
-export const initialState = {
+export const initialStateFixture = {
   pokemons: [],
   pokemonsFilter: [],
-  loading: false,
 };
 
-export const dataState = {
+export const dataStateWithPokemons = {
   pokemons: [
     {
       name: "Bulbasaur",
@@ -16,5 +15,33 @@ export const dataState = {
     },
   ],
   pokemonsFilter: [],
-  loading: false,
+};
+
+export const dataStateWithPokemonsFavorite = {
+  pokemons: [
+    {
+      favorite: true,
+      name: "Bulbasaur",
+      url: "https://pokeapi.co/api/v2/pokemon/1/",
+      sprites: {
+        front_default:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+      },
+    },
+  ],
+  pokemonsFilter: [],
+};
+
+export const dataStateWithPokemonsFilter = {
+  pokemons: [],
+  pokemonsFilter: [
+    {
+      name: "Bulbasaur",
+      url: "https://pokeapi.co/api/v2/pokemon/1/",
+      sprites: {
+        front_default:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+      },
+    },
+  ],
 };
